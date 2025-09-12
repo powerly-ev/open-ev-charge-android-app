59f58213baf741d193cb4913973ab8b2d956e004
ma7moud3ly <engma7moud3ly@gmail.com>
2025-09-13 02:08:11 +0300

    - fix order details screen ui - handlesstart chargingapi error response

--- core/model/src/main/java/com/powerly/core/model/api/BaseResponse.kt
+++ core/model/src/main/java/com/powerly/core/model/api/BaseResponse.kt
@@ -23,7 +23,7 @@ open class BaseResponse<T> {
 
     val isSuccess: Boolean get() = success == 1
     val hasData: Boolean get() = isSuccess && data != null
-    val hasOnlyData: Boolean get() = data != null
+    val hasOnlyData: Boolean get() = data != null && (success == null || success == 1)
 
     val getData: T get() = data!!
 }
